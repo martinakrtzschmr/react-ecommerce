b24a5f71a65b60f23477e57587543bef96ae8884
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-21 13:05:04 -0300

    Added button section to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import { Moon, ShoppingCart, Sun } from 'lucide-react';
 import { Button } from '../ui/button';
 import Container from '../ui/container';
 import { Link } from '@tanstack/react-router';
+import ProfileButton from '../ProfileButton';
 
 const routes = [
   {
@@ -42,6 +44,30 @@ const Header = (props: Props) => {
               </Button>
             ))}
           </nav>
+
+          <div className='flex items-center'>
+            <Button
+              variant='ghost'
+              size='icon'
+              className='mr-2'
+              aria-label='Shopping Cart'
+            >
+              <ShoppingCart className='h-6 w-6' />
+              <span className='sr-only'>Shopping Cart</span>
+            </Button>
+
+            <Button
+              variant='ghost'
+              size='icon'
+              aria-label='Toggle Theme'
+              className='mr-6'
+            >
+              <Sun className='h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+              <Moon className='absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+              <span className='sr-only'>Toggle Theme</span>
+            </Button>
+            <ProfileButton />
+          </div>
         </div>
       </Container>
     </header>
