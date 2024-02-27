b3b34ea492657751d9105c53ccae114983cbdf6e
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-27 15:37:23 -0300

    Add PROD variable for consistency

--- src/hooks/useListingData.ts
+++ src/hooks/useListingData.ts
@@ -2,12 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const DEV_ENV = import.meta.env.DEV;
+const PROD_ENV = import.meta.env.PROD;
 const API_URL = 'http://localhost:3500/data';
 // 'https://s3.us-west-2.amazonaws.com/cdn.number8.com/LA/listings.json';
 
 const fetchData = async () => {
   console.log('DEV_ENV: ', DEV_ENV);
-  if (DEV_ENV) {
+  if (DEV_ENV || PROD_ENV) { // TODO: Current code uses db.json always. Add backend API for this.
     const res = await fetch('/db.json');
     const data = await res.json();
     return data.data;
