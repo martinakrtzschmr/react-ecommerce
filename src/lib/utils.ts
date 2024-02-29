d0b14179233a81b61ac7df6b946e089b999be0b1
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2024-02-29 15:57:00 -0300

    Array dynamic filter based on search fields

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,36 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { ListItem } from '../../types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+// TODO: Make generic types. The function must accept any array of objects.
+export function filterObjects(array: ListItem[], filters: searchParamsProps) {
+  // Return complete array if there are no filters
+  if (Object.keys(filters).filter((key) => filters[key] !== '').length === 0)
+    return array;
+
+  // Clear empty filters
+  const currentFilters = Object.fromEntries(
+    Object.entries(filters).filter(([_, v]) => v !== '')
+  );
+
+  // Filter the array based on the provided filters
+  const meuArrayfiltrado = array.filter(
+    (el) => {
+      return Object.keys(currentFilters).every(key => {
+        if (typeof currentFilters[key] === 'string' && typeof el[key] === 'string') {
+          return el[key].toLowerCase().includes(currentFilters[key].toLowerCase());
+        }
+
+        return currentFilters[key] === el[key];
+      })
+    }
+  );
+
+  console.log('meuArrayfiltrado: ', meuArrayfiltrado);
+
+  return meuArrayfiltrado;
 }
